66084c36bda2fd979e34f11b3aaa9791f7869172
lorenzo <lollo.diaspro@gmail.com>
2026-01-16 12:07:11 +0100

    fix menu + projects

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -4,6 +4,9 @@ export default function About() {
         <section id="about" className="about-section section" >
             <div className="container">
                 <h2>About Me</h2>
+                <p>
+                I'm <span className="hl">Lorenzo Lunghi</span>. I studied computer engineering at <a href="https://www.uniroma1.it/en/pagina-strutturale/home">La Sapienza - University of Rome</a>, where I learned the mindset and skills needed to adapt to any type of language and framework.
+                </p>
                 <p>I am a passionate developer with experience in building web applications using modern technologies.</p>
             </div>
         </section>
--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -7,13 +7,19 @@ export default function Projects() {
       <h2>Projects</h2>
       <Holder>
         <ProjectCard>
-          <img src='logo.png' alt='Finance' />
+          <img src='finanza-che-conta.png' alt='Finance' />
           <Content>
             <h3>Finanza-che-conta</h3>
-            <p>Controlla...</p>
+            <p>
+              Finance is a long-term game, but some indicators are more concrete, current, and tangible than others. But who has time to monitor them regularly and remember to do so? 
+              Imagine this: a bot that sends a maximum of two notifications a week to an app you already use. Let the technology work for you, so you can focus on making informed decisions with the least amount of effort possible. Goodbye FOMO!
+            </p>
             <Tags>
               <Tag hashtag="Telegram Bot" />
+              <Tag hashtag="Scheduled" />
               <Tag hashtag="Finance" />
+              <Tag hashtag="Python" />
+              <Tag hashtag="Web Scraper" />
             </Tags>
           </Content>
           <CardActions visit="https://t.me/finanzacheconta" source="https://github.com/Loenus/finanza-che-conta" />
@@ -22,22 +28,36 @@ export default function Projects() {
           <img src='Unity' alt='Unity' /> {/* TODO recuperare dal pc con i file */}
           <Content>
             <h3>Unity Tutorial</h3>
-            <p>Divulgazione</p>
+            <p>
+              Conceived, produced, and published a series of Italian-language tutorials dedicated to video game development with Unity. 
+              The project has garnered hundreds of thousands of views, becoming a reference point for novice developers.
+            </p>
             <Tags>
               <Tag hashtag="YouTube" />
+              <Tag hashtag="Tutorial" />
               <Tag hashtag="Personal Brand" />
+              <Tag hashtag="Unity" />
+              <Tag hashtag="Adobe Premiere Pro" />
+              <Tag hashtag="OBS" />
             </Tags>
           </Content>
           <CardActions visit="https://www.youtube.com/@FactotumProduction/videos" />
         </ProjectCard>
         <ProjectCard>
-          <img src='dbPlain.png' alt='dbPlain' />
+          <img src='dbPlain3.png' alt='dbPlain' />
           <Content>
             <h3>dbPlain</h3>
-            <p>Test</p>
+            <p>
+              The application aims to act as a bridge between the end user and the database, facilitating and speeding up database management. 
+              Specifically, the application allows for the simultaneous management of multiple databases, allowing the viewing of their constituent tables and the subsequent insertion/deletion of data as needed.
+            </p>
             <Tags>
-              <Tag hashtag="Big Proj" />
-              <Tag hashtag="DB" />
+              <Tag hashtag="Docker Compose" />
+              <Tag hashtag="NodeJS" />
+              <Tag hashtag="Nginx" />
+              <Tag hashtag="CouchDB" />
+              <Tag hashtag="PostgreSQL" />
+              <Tag hashtag="Google API" />
             </Tags>
           </Content>
           <CardActions source="https://github.com/dbPlain/dbPlain" />
@@ -46,9 +66,14 @@ export default function Projects() {
           <img src='ruby-on-rails.png' alt='EventsAggregator' />
           <Content>
             <h3>EventsAggregator</h3>
-            <p>Test</p>
+            <p>
+              Development of an academic web application aimed at analyzing and using the main features and benefits of the Ruby on Rails framework. 
+              The project explored the MVC architecture and framework conventions to create a structured and extensible event aggregator.
+            </p>
             <Tags>
               <Tag hashtag="Ruby On Rails" />
+              <Tag hashtag="MVC" />
+              <Tag hashtag="Web Development" />
             </Tags>
           </Content>
           <CardActions source="https://github.com/EventsAggregator/EventsAggregator" />
